496e56a2cc371340aa45f68cc4f2c822ced8af48
nashaelxz12 <masha3l42@email.com>
2023-12-11 11:47:50 +0300

    4th commit

--- server.js
+++ server.js
@@ -11,3 +11,37 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
+// مسار آخر
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/about.html');
+});
+
+// مسار API
+app.get('/api/data', (req, res) => {
+    res.json({ message: 'Hello, this is API data!' });
+});
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// مسار يتفاعل مع بيانات POST
+app.post('/submit-form', (req, res) => {
+    const formData = req.body;
+    console.log('Received form data:', formData);
+    res.send('Form submitted successfully!');
+});
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/your-database-name', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Schema = mongoose.Schema;
+const exampleSchema = new Schema({
+    name: String,
+    age: Number
+});
+
+const ExampleModel = mongoose.model('Example', exampleSchema);
+
+// مثال على الاستعلام من قاعدة البيانات
+app.get('/get-examples', async (req, res) => {
+    const examples = await ExampleModel.find();
+    res.json(examples);
+});
