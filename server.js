50d20b6f14b6fb76546facdf55146857a0a3c94b
nashaelxz12 <masha3l42@email.com>
2023-12-12 11:08:29 +0300

    gh commit

--- server.js
+++ server.js
@@ -7,15 +7,65 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost/your-database-name', { useNewUrlParser: true, useUnifiedTopology: true });
-
-const Schema = mongoose.Schema;
-const exampleSchema = new Schema({
-    name: String,
-    age: Number
+mongoose.connect('mongodb://localhost/your-database-name', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
-const ExampleModel = mongoose.model('Example', exampleSchema);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'فشل الاتصال بقاعدة البيانات'));
+db.once('open', () => {
+    console.log('تم الاتصال بنجاح بقاعدة البيانات');
+});
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+});
+
+const User = mongoose.model('User', userSchema);
+
+const newUser = new User({
+    username: 'john_doe',
+    password: 'securepassword',
+});
+
+newUser.save((err, user) => {
+    if (err) return console.error(err);
+    console.log('تمت إضافة مستخدم جديد:', user);
+});
+
+User.find({})
+    .then((users) => {
+        console.log('جميع المستخدمين:', users);
+    })
+    .catch((error) => {
+        console.error('حدث خطأ أثناء البحث عن المستخدمين:', error);
+    });
+
+User.findOne({ username: 'john_doe' })
+    .then((user) => {
+        console.log('المستخدم المحدد:', user);
+    })
+    .catch((error) => {
+        console.error('حدث خطأ أثناء البحث عن المستخدم:', error);
+    });
+
+User.updateOne({ username: 'john_doe' }, { password: 'newpassword' })
+    .then((result) => {
+        console.log('تم تحديث المستخدم:', result);
+    })
+    .catch((error) => {
+        console.error('حدث خطأ أثناء تحديث المستخدم:', error);
+    });
+
+User.deleteOne({ username: 'john_doe' })
+    .then((result) => {
+        console.log('تم حذف المستخدم:', result);
+    })
+    .catch((error) => {
+        console.error('حدث خطأ أثناء حذف المستخدم:', error);
+    });
 
 app.get('/api/examples', async (req, res) => {
     try {
@@ -27,12 +77,12 @@ app.get('/api/examples', async (req, res) => {
 });
 
 app.get('/artists', (req, res) => {
-    // Assuming you have a list of artists, replace the comment with the actual logic to fetch artists from the database
-    const artists = /* Logic to fetch artists from the database */;
+    // قم بتنفيذ اللوجيك هنا لاسترجاع الفنانين من قاعدة البيانات
+    const artists = /* لوجيك لاسترجاع الفنانين من قاعدة البيانات */;
 
-    res.render('artists', { artists }); // Assuming you have a template engine (like EJS) for rendering HTML with dynamic data
+    res.render('artists', { artists }); // يفترض أن يكون لديك محرك قوالب (مثل EJS) لتقديم HTML مع البيانات الديناميكية
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`الخادم يعمل على http://localhost:${port}`);
 });
